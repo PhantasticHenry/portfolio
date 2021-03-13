6a40e07d4a9443213cc87f2d36b5536e4b3ec64a
github username <github email address>
2021-03-12 20:13:16 -0800

    add  router switch route

--- project/src/App.js
+++ project/src/App.js
@@ -1,101 +1,17 @@
 import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navigation from "./components/Navigation";
+import AboutMe from "./features/AboutMe";
 
 function App() {
   return (
     <div className="App">
-      <div id="phone">
-        <div id="camera"></div>
-        <div id="speakers"> </div>
-        <div id="screen">
-          <div className="container">
-            <div className="icon-container">
-              <div className="envelope">
-                <div className="envelope-left"></div>
-                <div className="envelope-right"></div>
-              </div>
-              <div className="envelope-top"></div>
-              <h2>Email Me</h2>
-            </div>
-            <div className="container">
-              <div className="icon-container">
-                <div className="portfolio orange"></div>
-                <div className="portfolio yellow"></div>
-                <div className="portfolio green"></div>
-                <div className="portfolio lightBlue"></div>
-                <div className="portfolio blue"></div>
-                <div className="portfolio purple"></div>
-                <div className="portfolio violet"></div>
-                <div className="portfolio red"></div>
-                <h2>Portfolio</h2>
-              </div>
-            </div>
-          </div>
-          {/* <img src="" alt="photo" id="photo" />
-          <h1 className="title">henry phan</h1>
-          <h2 className="sub-title">full-stack web developer</h2>
-          <section>
-            <h3 className="what">What I Do</h3>
-            <p>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-              Voluptates ipsam dolor delectus! Quas aliquid, ea ullam, maxime ex
-              beatae reiciendis pariatur recusandae dolore ipsa non iure
-              explicabo quibusdam rerum illum.
-            </p>
-          </section>
-          <section>
-            <h3 className="how">How It's Done</h3>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Doloremque dignissimos dolor illo at sit nisi quod veritatis optio
-              rem incidunt blanditiis eaque magnam, non, laboriosam id
-              asperiores. Facilis, cumque officiis.
-            </p>
-          </section>
-          <section>
-            <h3 className="about-me">About Me</h3>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Id iusto
-              eos ipsum incidunt veniam facere assumenda nemo nobis quia optio
-              magnam quas atque, omnis, enim quam iure ut minus ad.
-            </p>
-          </section> */}
-        </div>
-        <div id="homebutton"></div>
-      </div>
-      {/* monitor */}
-
-      <div class="iMac">
-        <div class="body">
-          <div class="shine"></div>
-          <div class="screen">
-            <div className="container">
-              <div className="icon-container">
-                <div className="envelope">
-                  <div className="envelope-left"></div>
-                  <div className="envelope-right"></div>
-                </div>
-                <div className="envelope-top"></div>
-                <h2>Email Me</h2>
-              </div>
-              <div className="container">
-                <div className="icon-container">
-                  <div className="portfolio orange"></div>
-                  <div className="portfolio yellow"></div>
-                  <div className="portfolio green"></div>
-                  <div className="portfolio lightBlue"></div>
-                  <div className="portfolio blue"></div>
-                  <div className="portfolio purple"></div>
-                  <div className="portfolio violet"></div>
-                  <div className="portfolio red"></div>
-                  <h2>Portfolio</h2>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="logo"></div>
-        </div>
-        <div class="stand"></div>
-      </div>
+      <Router>
+        <Navigation />
+        <Switch>
+          <Route exact path="/about-me" component={AboutMe} />
+        </Switch>
+      </Router>
     </div>
   );
 }
