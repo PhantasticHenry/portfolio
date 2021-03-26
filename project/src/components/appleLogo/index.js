4571d6c95b299a135e0f64ea7e7b20252baa7114
github username <github email address>
2021-03-25 19:38:18 -0700

    AppleLogo component

---
+++ project/src/components/appleLogo/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./AppleLogo.css";
+
+function AppleLogo() {
+  return (
+    <div className="appleLogo_container">
+      <div className="appleLogo"></div>
+    </div>
+  );
+}
+
+export default AppleLogo;
