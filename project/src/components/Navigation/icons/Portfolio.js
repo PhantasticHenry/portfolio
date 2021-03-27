f5f3b5a721d507811f3b076656a1a6df298c3549
github username <github email address>
2021-03-26 17:48:54 -0700

    refactor icon to component

---
+++ project/src/components/Navigation/icons/Portfolio.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Portfolio() {
+	return (
+		<div className="icon-container">
+			<div className="portfolio orange"></div>
+			<div className="portfolio green"></div>
+			<div className="portfolio yellow"></div>
+			<div className="portfolio lightBlue"></div>
+			<div className="portfolio blue"></div>
+			<div className="portfolio purple"></div>
+			<div className="portfolio violet"></div>
+			<div className="portfolio red"></div>
+			<h2>Portfolio</h2>
+		</div>
+	);
+}
+
+export default Portfolio;
