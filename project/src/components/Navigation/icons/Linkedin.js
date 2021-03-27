5019687a81cc28ce43810495b168da27939573ca
github username <github email address>
2021-03-26 21:33:13 -0700

    create Linkedin icon component

---
+++ project/src/components/Navigation/icons/Linkedin.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Linkedin() {
+	return (
+		<div className="icon-container">
+			<div className="linkedin">in</div>
+		</div>
+	);
+}
+
+export default Linkedin;
