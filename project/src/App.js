52edfcd3e805355774cd411fb894869cdd80efd4
github username <github email address>
2021-03-30 00:22:03 -0700

    removed dependecy

--- project/src/App.js
+++ project/src/App.js
@@ -3,41 +3,57 @@ import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navigation from "./components/Navigation";
 import AboutMe from "./features/AboutMe";
+import OnButton from "./components/Button/OnButton";
 
 function App() {
-	const [isDesktop, setIsDesktop] = useState(window.innerWidth > 1024);
+	const [isDesktop, setIsDesktop] = useState(window.innerWidth > 1500);
+	const [isMobile, setIsMobile] = useState(window.innerWidth < 800);
+	// const [isDesktop, setIsDesktop] = useState(window.innerWidth > 1024);
 
 	const updateMedia = () => {
-		setIsDesktop(window.innerWidth > 1024);
+		setIsDesktop(window.innerWidth > 1500);
+		setIsMobile(window.innerWidth < 800);
+		// setIsDesktop(window.innerWidth > 1024);
 	};
 
 	useEffect(() => {
 		window.addEventListener("resize", updateMedia);
+		console.log("innerWidth = ", window.innerWidth);
 		return () => {
 			window.removeEventListener("resize", updateMedia);
 		};
-	}, updateMedia);
+	});
 
 	return (
 		<div className="device-container">
-			<div className={isDesktop ? "device macbook" : "device ipad silver"}>
-				<div className="top-bar"></div>
-				<div className="sleep"></div>
-				<div className="volume"></div>
-				<div className="camera"></div>
-				<div className="sensor"></div>
-				<div className="speaker"></div>
-				<div className="screen">
-					<Router>
-						<Navigation />
-						<Switch>
-							<Route exact path="/about-me" component={AboutMe} />
-						</Switch>
-					</Router>
+			<OnButton />
+			{isMobile ? (
+				<Router>
+					<Navigation />
+					<Switch>
+						<Route exact path="/about-me" component={AboutMe} />
+					</Switch>
+				</Router>
+			) : (
+				<div className={isDesktop ? "device macbook" : "device ipad silver"}>
+					<div className="top-bar"></div>
+					<div className="sleep"></div>
+					<div className="volume"></div>
+					<div className="camera"></div>
+					<div className="sensor"></div>
+					<div className="speaker"></div>
+					<div className="screen">
+						<Router>
+							<Navigation />
+							<Switch>
+								<Route exact path="/about-me" component={AboutMe} />
+							</Switch>
+						</Router>
+					</div>
+					<div className="home"></div>
+					<div className="bottom-bar"></div>
 				</div>
-				<div className="home"></div>
-				<div className="bottom-bar"></div>
-			</div>
+			)}
 		</div>
 	);
 }
