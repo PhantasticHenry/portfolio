1fbb41457c3a72eedc9305c994fa175d86abcd3b
github username <github email address>
2021-04-07 21:45:39 -0700

    add CSSTransition to Navigation component

--- project/src/components/Navigation/index.js
+++ project/src/components/Navigation/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Navigation.css";
 import Envelope from "./icons/Envelope";
 import Portfolio from "./icons/Portfolio";
@@ -6,17 +6,40 @@ import FindMe from "./icons/FindMe";
 import Me from "./icons/Me";
 import Linkedin from "./icons/Linkedin";
 import Twitter from "./icons/Twitter";
+import { NavLink } from "react-router-dom";
+import { CSSTransition } from "react-transition-group";
 
 function Navigation() {
+	const timeout = { enter: 800, exit: 400 };
+	const navLinks = [
+		{ to: "/about", Icon: Me },
+		{ to: "/email", Icon: Envelope },
+	];
 	return (
-		<div id="Navigation">
-			<Envelope />
+		<nav id="Navigation" className="flex flex-jc-se flex-ac-center flex-wrap">
+			{navLinks.map(({ to, Icon }) => (
+				<CSSTransition
+					in={to != null}
+					timeout={timeout}
+					unmountOnExit
+					classNames="tIn"
+				>
+					<NavLink to={to} activeClassName="selected">
+						<Icon />
+					</NavLink>
+				</CSSTransition>
+			))}
+			{/* <NavLink to="/about" activeClassName="selected">
+				<Me />
+			</NavLink>
+			<NavLink to="/email" activeClassName="selected">
+				<Envelope />
+			</NavLink> */}
 			<Portfolio />
 			<FindMe />
-			<Me />
 			<Linkedin />
 			<Twitter />
-		</div>
+		</nav>
 	);
 }
 
