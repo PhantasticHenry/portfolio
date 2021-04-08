4fabfacaa36891eef05087882e4b1f326a02d9bc
github username <github email address>
2021-04-07 21:44:32 -0700

    add browser router and redirect / to power-button

--- project/src/index.js
+++ project/src/index.js
@@ -2,10 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+	<React.StrictMode>
+		{/* <App /> */}
+		<Router>
+			<Route exact path="/">
+				<Redirect to="/power-button" />
+			</Route>
+			<App />
+		</Router>
+	</React.StrictMode>,
+	document.getElementById("root")
 );
