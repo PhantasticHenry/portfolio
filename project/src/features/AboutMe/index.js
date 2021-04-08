6ced376da444bd5c4a4d7da1bcc3535b5d7decaa
github username <github email address>
2021-04-07 21:46:41 -0700

    simple template for AboutMe component

--- project/src/features/AboutMe/index.js
+++ project/src/features/AboutMe/index.js
@@ -1,39 +1,39 @@
 import React from "react";
 
 function AboutMe() {
-  return (
-    <div className="About-Me">
-      <img src="" alt="" id="photo" />
-      <h1 className="title">henry phan</h1>
-      <h2 className="sub-title">full-stack web developer</h2>
-      <section>
-        <h3 className="what">What I Do</h3>
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates
-          ipsam dolor delectus! Quas aliquid, ea ullam, maxime ex beatae
-          reiciendis pariatur recusandae dolore ipsa non iure explicabo
-          quibusdam rerum illum.
-        </p>
-      </section>
-      <section>
-        <h3 className="how">How It's Done</h3>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
-          dignissimos dolor illo at sit nisi quod veritatis optio rem incidunt
-          blanditiis eaque magnam, non, laboriosam id asperiores. Facilis,
-          cumque officiis.
-        </p>
-      </section>
-      <section>
-        <h3 className="about-me">About Me</h3>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Id iusto eos
-          ipsum incidunt veniam facere assumenda nemo nobis quia optio magnam
-          quas atque, omnis, enim quam iure ut minus ad.
-        </p>
-      </section>
-    </div>
-  );
+	return (
+		<div className="About-Me">
+			<img src="" alt="" id="photo" />
+			<h1 className="title">henry phan</h1>
+			<h2 className="sub-title">full-stack web developer</h2>
+			<section>
+				<h3 className="what">What I Do</h3>
+				<p>
+					Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates
+					ipsam dolor delectus! Quas aliquid, ea ullam, maxime ex beatae
+					reiciendis pariatur recusandae dolore ipsa non iure explicabo
+					quibusdam rerum illum.
+				</p>
+			</section>
+			<section>
+				<h3 className="how">How It's Done</h3>
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
+					dignissimos dolor illo at sit nisi quod veritatis optio rem incidunt
+					blanditiis eaque magnam, non, laboriosam id asperiores. Facilis,
+					cumque officiis.
+				</p>
+			</section>
+			<section>
+				<h3 className="about-me">About Me</h3>
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Id iusto eos
+					ipsum incidunt veniam facere assumenda nemo nobis quia optio magnam
+					quas atque, omnis, enim quam iure ut minus ad.
+				</p>
+			</section>
+		</div>
+	);
 }
 
 export default AboutMe;
