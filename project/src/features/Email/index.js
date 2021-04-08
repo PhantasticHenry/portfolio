e7a1b683382c6dcf892dc4332fbcc98067414e73
github username <github email address>
2021-04-07 21:47:47 -0700

    add Email icon component

---
+++ project/src/features/Email/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Email.css";
+
+function Email() {
+	return (
+		<div>
+			<h1>Email Component to email yours truly</h1>
+		</div>
+	);
+}
+
+export default Email;
