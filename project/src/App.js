7bd5493d2250d40288ab4642b802c3594bd40155
github username <github email address>
2021-04-07 21:42:46 -0700

    media conditional

--- project/src/App.js
+++ project/src/App.js
@@ -1,19 +1,20 @@
 import "./App.css";
-import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import React, { useState, useEffect, useRef } from "react";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 import Navigation from "./components/Navigation";
 import AboutMe from "./features/AboutMe";
-import OnButton from "./components/Button/OnButton";
+import Cover from "./features/Cover";
+import Email from "./features/Email";
+import { CSSTransition } from "react-transition-group";
 
 function App() {
+	const timeout = { enter: 800, exit: 400 };
 	const [isDesktop, setIsDesktop] = useState(window.innerWidth > 1500);
 	const [isMobile, setIsMobile] = useState(window.innerWidth < 800);
-	// const [isDesktop, setIsDesktop] = useState(window.innerWidth > 1024);
 
 	const updateMedia = () => {
 		setIsDesktop(window.innerWidth > 1500);
 		setIsMobile(window.innerWidth < 800);
-		// setIsDesktop(window.innerWidth > 1024);
 	};
 
 	useEffect(() => {
@@ -24,36 +25,66 @@ function App() {
 		};
 	});
 
+	function deviceClassName() {
+		// consider switch case ???
+		if (isMobile) {
+			return "mobile";
+		} else if (isDesktop) {
+			return "device macbook";
+		} else {
+			return "device ipad silver";
+		}
+	}
+
+	// let routes = (
+	// 	<Switch location={location}>
+	// 		<Route exact path="/power-button" component={Cover} />
+	// 		<Route exact path="/home" children={<Navigation />} />
+	// 		<Route exact path="/about-me" children={<AboutMe />} />
+	// 	</Switch>
+	// );
+	const routes = [
+		{ path: "/power-button", Component: Cover },
+		{ path: "/home", Component: Navigation },
+		{ path: "/about", Component: AboutMe },
+		{ path: "/email", Component: Email },
+	];
+
 	return (
 		<div className="device-container">
-			<OnButton />
-			{isMobile ? (
-				<Router>
-					<Navigation />
-					<Switch>
-						<Route exact path="/about-me" component={AboutMe} />
-					</Switch>
-				</Router>
-			) : (
-				<div className={isDesktop ? "device macbook" : "device ipad silver"}>
-					<div className="top-bar"></div>
-					<div className="sleep"></div>
-					<div className="volume"></div>
-					<div className="camera"></div>
-					<div className="sensor"></div>
-					<div className="speaker"></div>
-					<div className="screen">
-						<Router>
-							<Navigation />
-							<Switch>
-								<Route exact path="/about-me" component={AboutMe} />
-							</Switch>
-						</Router>
-					</div>
-					<div className="home"></div>
-					<div className="bottom-bar"></div>
+			<div className={deviceClassName()}>
+				{isDesktop ? (
+					<>
+						<div className="top-bar"></div>
+						<div className="sleep"></div>
+						<div className="volume"></div>
+						<div className="camera"></div>
+						<div className="sensor"></div>
+						<div className="speaker"></div>
+					</>
+				) : null}
+				<div className="screen">
+					{routes.map(({ path, Component }) => (
+						<Route key={path} exact path={path}>
+							{({ match }) => (
+								<CSSTransition
+									in={match != null}
+									timeout={timeout}
+									// timeout={500}
+									classNames="page"
+									unmountOnExit
+								>
+									<div className="page">
+										<Component />
+									</div>
+								</CSSTransition>
+							)}
+						</Route>
+					))}
 				</div>
-			)}
+				<div className="home"></div>
+				<div className="bottom-bar"></div>
+			</div>
 		</div>
 	);
 }
