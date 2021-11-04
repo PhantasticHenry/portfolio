7e69726e318d2424d4b0d7c6885ac501855bc391
github username <github email address>
2021-11-03 23:14:59 -0700

    wrap porfolio icon shapes with sahpe-container div

--- project/src/components/Navigation/icons/Portfolio.js
+++ project/src/components/Navigation/icons/Portfolio.js
@@ -4,14 +4,16 @@ function Portfolio() {
 	return (
 		<div className="icon-container">
 			<div className="icon">
-				<div className="portfolio orange"></div>
-				<div className="portfolio green"></div>
-				<div className="portfolio yellow"></div>
-				<div className="portfolio lightBlue"></div>
-				<div className="portfolio blue"></div>
-				<div className="portfolio purple"></div>
-				<div className="portfolio violet"></div>
-				<div className="portfolio red"></div>
+				<div className="shape-container">
+					<div className="portfolio orange"></div>
+					<div className="portfolio green"></div>
+					<div className="portfolio yellow"></div>
+					<div className="portfolio lightBlue"></div>
+					<div className="portfolio blue"></div>
+					<div className="portfolio purple"></div>
+					<div className="portfolio violet"></div>
+					<div className="portfolio red"></div>
+				</div>
 				<h3>Portfolio</h3>
 			</div>
 		</div>
